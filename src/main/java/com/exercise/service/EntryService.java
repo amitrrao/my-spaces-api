bfb61e78a26cfc7d651ef34fca7f41b8c71c6ef4
arunima-goel <amith.nie@gmail.com>
2018-01-13 00:47:10 -0600

    Cleaned up some code by moving test data to a separate class.

--- src/main/java/com/exercise/service/EntryService.java
+++ src/main/java/com/exercise/service/EntryService.java
@@ -1,52 +1,20 @@
 package com.exercise.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.exercise.builder.EntryBuilder;
+import com.exercise.data.TestData;
 import com.exercise.item.Item;
 
 /**
  * 
  * EntryService is a service class that implements REST methods for Entry related information.
- *
+ * All the data for this class is coming from TestData. In production, it would ideally come from a database.
+ * 
  */
 @Service
 public class EntryService {
-	
-	private List<Item> entries = new ArrayList<>(Arrays.asList(
-			new EntryBuilder(
-					"Hello, World!", 
-					"A Hello World is a common starting point for learning a new language or platform.", 
-					"Body 1", 
-					"5KsDBWseXY6QegucYAoacS1", 
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU",
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU").build(),
-			new EntryBuilder(
-					"Tutorial 1", 
-					"In this tutorial, we will dive deeper into how to leverage the new platform.", 
-					"Body 2", 
-					"5KsDBWseXY6QegucYAoacS2", 
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU",
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU2").build(),
-			new EntryBuilder(
-					"Tutorial 2", 
-					"In this tutorial, we will provide an example deployment.", 
-					"Body 3", 
-					"5KsDBWseXY6QegucYAoacS3", 
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU",
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU2").build()
-			));
 	
 	/**
 	 * A method to get all entries for a given spaceId.
@@ -55,7 +23,7 @@ public class EntryService {
 	 * @return a list of all Entry instances.
 	 */
 	public List<Item> getAllEntriesBySpace(String spaceId) {
-		return entries;
+		return TestData.ENTRIES;
 	}
 	
 	/**
@@ -66,7 +34,7 @@ public class EntryService {
 	 * @return a single Entry instance
 	 */
 	public Item getEntryBySpaceIdAndEntryId(String spaceId, String entryId) {
-		return entries.stream().filter(e -> e.getSys()
+		return TestData.ENTRIES.stream().filter(e -> e.getSys()
 				.getId()
 				.equals(entryId))
 				.findFirst()
--- src/main/java/com/exercise/service/SpaceService.java
+++ src/main/java/com/exercise/service/SpaceService.java
@@ -1,49 +1,20 @@
 package com.exercise.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.exercise.builder.SpaceBuilder;
+import com.exercise.data.TestData;
 import com.exercise.item.Item;
 
 /**
  * 
  * SpaceService is a service class that implements REST methods for Space related information.
- *
+ * All the data for this class is coming from TestData. In production, it would ideally come from a database.
+ * 
  */
 @Service
 public class SpaceService {
-
-	private List<Item> spaces = new ArrayList<>(Arrays.asList(
-			new SpaceBuilder(
-					"My First Space", 
-					"This space was created by Alanna Atlassian as an example of how to create a space and relate assets and entries to it.", 
-					"yadj1kx9rmg0", 
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU", 
-					new Date(), 
-					"4FLrUHftHW3v2BLi9fzfjU").build(),
-			new SpaceBuilder(
-					"My Second Space",
-					"",
-					"yadj1kx9rmg01",
-					new Date(),
-					"4FLrUHftHW3v2BLi9fzfjU",
-					new Date(),
-					"4FLrUHftHW3v2BLi9fzfjU").build(),
-			new SpaceBuilder(
-					"My Third Space",
-					"",
-					"yadj1kx9rmg02",
-					new Date(),
-					"4FLrUHftHW3v2BLi9fzfjU",
-					new Date(),
-					"4FLrUHftHW3v2BLi9fzfjU").build()
-			));
 	
 	/**
 	 * A method to get all spaces in the system
@@ -51,7 +22,7 @@ public class SpaceService {
 	 * @return a list of all Space instances
 	 */
 	public List<Item> getAllSpaces() {
-		return spaces;
+		return TestData.SPACES;
 	}
 	
 	/**
@@ -61,7 +32,7 @@ public class SpaceService {
 	 * @return a single Space instance
 	 */
 	public Item getSpace(String spaceId) {
-		return spaces.stream().filter(s -> s.getSys()
+		return TestData.SPACES.stream().filter(s -> s.getSys()
 				.getId()
 				.equals(spaceId))
 				.findFirst()
