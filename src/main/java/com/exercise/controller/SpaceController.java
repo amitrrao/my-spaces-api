5641a32fe09bc3955e399bf3b51a451fd7be9917
arunima-goel <amith.nie@gmail.com>
2018-01-13 00:26:52 -0600

    Added a logger.

--- src/main/java/com/exercise/controller/SpaceController.java
+++ src/main/java/com/exercise/controller/SpaceController.java
@@ -2,6 +2,8 @@ package com.exercise.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +36,8 @@ public class SpaceController { //implements ErrorController {
 	
 	@RequestMapping("/spaces")
 	public List<Item> getAllSpaces() {
+		final Logger logger = LoggerFactory.getLogger(SpaceController.class);
+		logger.info("Getting all spaces");
 		return spaceService.getAllSpaces();
 	}
 	
