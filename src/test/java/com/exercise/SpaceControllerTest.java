19817dc42e70fefa0d55238bd2ff3bd11fcc0fb6
arunima-goel <amith.nie@gmail.com>
2018-01-14 23:52:44 -0600

    still works

--- src/test/java/com/exercise/SpaceControllerTest.java
+++ src/test/java/com/exercise/SpaceControllerTest.java
@@ -1,10 +1,15 @@
 package com.exercise;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.exercise.service.SpaceService;
 
@@ -12,11 +17,21 @@ import com.exercise.service.SpaceService;
 @SpringBootTest
 public class SpaceControllerTest {
 	
+	private MockMvc mockMvc;
+	
 	@MockBean
-	private SpaceService spaceServicerMock;
+	private SpaceService mockSpaceService;
 
+	@Autowired
+    private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
 	@Test
-	public void test() {
+	public void getAllSpaces_Success() {
+		
 		System.out.println("Aru: Test");
 	}
 
